cd470cb3eb157f87c25826fdbcda49ed3eb98c22
qiaogeng.lin <qiaogeng.lin@jimicn.com>
2019-08-10 00:31:06 +0800

    更新标签代码

--- app/src/main/java/com/lqg/Image/MainActivity.java
+++ app/src/main/java/com/lqg/Image/MainActivity.java
@@ -1,12 +1,9 @@
 package com.lqg.Image;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
-
-import com.lqg.Image.bean.LabelBean;
 
 public class MainActivity extends AppCompatActivity {
 
